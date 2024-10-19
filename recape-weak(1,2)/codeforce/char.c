71113bc5fc3cd188902a2ef506831794f339b07c
sarwar-asik <sarwarasik@gmail.com>
2024-10-19 18:10:41 +0600

    u

--- recape-weak(1,2)/codeforce/char.c
+++ recape-weak(1,2)/codeforce/char.c
@@ -1,8 +1,31 @@
+// Online C compiler to run C program online
 #include <stdio.h>
 
 int main() {
-    printf("first init code!\n");
+    // Write C code here
+    // printf("Try programiz.pro");
+    long int A,B;
+    char S;
+    scanf("%ld",&A);
+    
+    scanf(" %c",&S);
+    scanf("%ld",&B);
+    // printf("\n %ld and  %c and  %ld",A,S,B);
+    
+    if(S="<" && A<B){
+       printf("Right");
+    }
+    else if(S=">"&&A>B){
+        printf("Right");
+    }
+     else if(S="="&&A==B){
+        printf("Right");
+    }
+    else {
+        printf("Wrong");
+    }
+    
+       
 
     return 0;
-
 }
