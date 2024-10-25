7ce2e46fc672d10ca39a264c0ba24f40bfea71ab
sarwar-asik <sarwarasik@gmail.com>
2024-10-25 08:31:22 +0600

    module-11: string method done

---
+++ weak-3/module-11(StringFrequency)/string.lexicographical.order.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<string.h>
+int main() {
+
+
+char str1[10]= "air";
+char str2[10]= "airbus";
+int compare= strcmp(str1,str2);
+printf("%d",compare);
+
+    return 0;
+}
