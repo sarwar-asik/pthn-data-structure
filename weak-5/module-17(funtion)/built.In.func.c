5981377550103f346fbb028f5da5cb47dda5b01b
sarwar-asik <sarwarasik@gmail.com>
2024-11-06 07:31:25 +0600

    module017: complete

---
+++ weak-5/module-17(funtion)/built.In.func.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double a = 10.47;
+    printf("%f\n", ceil(a)); // Use %f for printing double
+
+    // Uncomment these lines to use them:
+    // printf("%f\n", floor(a));
+    // printf("%f\n", sqrt(a));
+    // printf("%f\n", pow(2, 3));
+    // printf("%f\n", log(2));
+
+    return 0;
+}
---
+++ weak-5/module-17(funtion)/func.pointer.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+
+void swap(int *a ,int *b){
+int temp = *a ;
+*a= *b;
+*b=temp;
+printf("func pointer %p %p \n",a,b);
+    
+}
+int main() {
+
+    int a = 10;
+    int b=20;
+      swap(&a,&b);
+      printf("a=%d b=%d\n",a,b);
+
+  return 0;
+}
