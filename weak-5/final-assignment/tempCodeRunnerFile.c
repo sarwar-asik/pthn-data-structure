e2c02a8fe15a98a3e92db73f818788d28e940a12
sarwar-asik <sarwarasik@gmail.com>
2024-11-11 23:51:25 +0600

    assignment-3:  #complete

--- weak-5/final-assignment/tempCodeRunnerFile.c
+++ weak-5/final-assignment/tempCodeRunnerFile.c
@@ -1,6 +1,3 @@
- for(int i=0;i<N;i++){
-//       for(int j=0;j<M;j++){
-//           printf("%d ",matrix[i][j]);
-//       }
-//       printf("\n");
-//   }
+ printf(a[i]);
+                printf(b[i]);
+                printf(c[i]);
