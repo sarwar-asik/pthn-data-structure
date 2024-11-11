5b1baa3973d3e44d262304491a0a03864c404184
sarwar-asik <sarwarasik@gmail.com>
2024-11-11 08:11:19 +0600

    assignment-3: swap #complete

--- weak-5/final-assignment/3.swap.matrix.c
+++ weak-5/final-assignment/3.swap.matrix.c
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+// https://www.hackerrank.com/contests/b6-final-c/challenges/swap-in-matrix/submissions/code/1385171018
 
 int main()
 {
@@ -13,19 +14,39 @@ int main()
       }
   }
   
-  printf("Before Swapping\n");
+//   printf("Before Swapping\n");
 
+//   for(int i=0;i<N;i++){
+//       for(int j=0;j<M;j++){
+//           printf("%d ",matrix[i][j]);
+//       }
+//       printf("\n");
+//   }
+
+// printf("Swapping\n");
+ 
+ //! Swapping the first and last column
+   for(int i=0;i<N;i++){
+   int temCol= matrix[i][0];   ///! first col
+   matrix[i][0]= matrix[i][M-1];
+   matrix[i][M-1]= temCol;   //! last column    
+  }
+  
+//   ! Swapping the first and last row
+// printf("N= %d\n",N);
+for(int i=0;i<M;i++){
+   int temRow= matrix[0][i];   ///! first row
+   matrix[0][i]= matrix[N-1][i];
+   matrix[N-1][i]= temRow;   //! last row
+// printf("%d",matrix[0][i]);   ///first row
+// printf("%d",matrix[N-1][i]);   /// last row
+}
+
+
+//   printf("\nAfter Swapping\n");
   for(int i=0;i<N;i++){
       for(int j=0;j<M;j++){
           printf("%d ",matrix[i][j]);
-      }
-      printf("\n");
-  }
-  
-  printf("After Swapping\n");
-  for(int i=0;i<N;i++){
-      for(int j=0;j<M;j++){
-          printf("%d ",matrix[j][i]);
       }
       printf("\n");
   }
