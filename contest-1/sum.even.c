a7e8a829daf8a9086d9b0de67a3e5deea3661f62
sarwar-asik <sarwarasik@gmail.com>
2024-11-16 22:23:30 +0600

    contest-1:  sum.even problem #solved

--- contest-1/sum.even.c
+++ contest-1/sum.even.c
@@ -1,16 +1,20 @@
 // Online C compiler to run C program online
 #include <stdio.h>
-// https://www.hackerrank.com/contests/phitron-monthly-contest-a-batch-6/challenges/sum-of-evens-3
+
+/// https://www.hackerrank.com/contests/phitron-monthly-contest-a-batch-6/challenges/sum-of-evens-3
+
 int main() {
-    // Write C code here
-    // printf("Try programiz.pro\n");
+
+    /// Write C code here
     long long N;
     scanf("%lld",&N);
     long long sum=0;
     for(long long i=1; i<=N;i++){
         if(i%2==0){
             // printf("i= %lld sum=%lld\n",i,sum);
-            sum = sum+ i;
+            // printf("%lld\n",i);
+            // 
+            sum = sum+ i;           
         }
     }
     printf("%lld",sum);
