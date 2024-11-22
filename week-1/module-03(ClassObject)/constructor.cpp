dd8e7b7e11f4f6676828b9bd1ad66ef81b3a8756
sarwar-asik <sarwarasik@gmail.com>
2024-11-23 00:22:10 +0600

    midterm: complete assignment-1

--- week-1/module-03(ClassObject)/constructor.cpp
+++ week-1/module-03(ClassObject)/constructor.cpp
@@ -3,12 +3,19 @@ using namespace std;
 
    class Student {
         public:
-        char name[100]; /// taken 100 bytes memory
+        int cls;
         int roll; /// taken 4 bytes
         double gpa; /// taken 8 bytes
+        Student(int cls,int roll,double gpa){
+            this->roll=roll;
+            this->cls=cls;
+            this->gpa=gpa;
+        }
     };
 int main(){ 
 
 
+    Student karim(5,20,2.44);
+    cout << karim.roll << " " << karim.cls << " " << karim.gpa << endl;
     return 0;
 }
