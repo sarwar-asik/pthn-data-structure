c132836800f5758108cfb85aa6b08b8efad59030
sarwar-asik <sarwarasik@gmail.com>
2024-11-30 09:13:39 +0600

    parctice-2: solved @codeforce

---
+++ week-2/practice-1/replace.word.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+// https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/V
+int main() {
+    // main code here
+    string S;
+    cin >> S;
+    cout << S << endl;
+
+    S.replace(S.begin(),S.end(),"EGYP");
+    cout << S << endl;
+    return 0;
+}
