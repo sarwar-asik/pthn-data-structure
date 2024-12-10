3a43ef1c7651c0f819bd1eea7c312862a25fdc31
sarwar-asik <sarwarasik@gmail.com>
2024-12-10 08:50:07 +0600

    branch cleaned

--- week-2/finalExam/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
- if(a.eng_marks > b.eng_marks){
-   return false;
- }
- else if(a.eng_marks == b.eng_marks && a.math_marks > b.math_marks){
-   return false;
- }
- else if(a.eng_marks == b.eng_marks && a.math_marks == b.math_marks && a.id > b.id){
-   return false;
- }
--- week-2/module-5(String)/tempCodeRunnerFile.cpp
+++
@@ -1,4 +0,0 @@
-    // for(int i=0;i<s.size();i++){
-    //     cout<<s[i]<<" ";
-    // }
-    // cout <<endl<< *s.begin() << endl;
