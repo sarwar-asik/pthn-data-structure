b0423424e5be3ddf81c649d84515997155b65df0
sarwar-asik <sarwarasik@gmail.com>
2024-12-22 10:49:14 +0600

    complte: module-6

---
+++ Get_Difference.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+    int val;
+    Node* next;   // pointer of the next address
+    Node(int val){
+      this->val=val;
+      this->next=NULL;
+    }
+};
+
+
+int main() {
+    // main code here
+
+    return 0;
+}
