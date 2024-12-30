1baa9aaa548f3b07112b55a5ce3686c527b0aa7e
sarwar-asik <sarwarasik@gmail.com>
2024-12-30 18:18:27 +0600

    module-10: stl constructor

---
+++ week-3(doubly)/module-10(STL_list&Cycle_detection)/1.STL_list_constructor.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    // list<int> l;  // ! empty list
+    // list<int> l(10); // ! list of size 10
+    // list <int> l(10,99); // ! list of size 10 with 99 in each element
+    list <int> l ={1,2,4,5,76}; // ! list of size 10 with 99 in each element
+   
+    list <int>L_2(l); // ! copy constructor
+   
+    int arr[5]={1,2,3,4,5};
+    list <int> l2(arr,arr+5); // ! copy constructor
+    
+    cout<<"size = "<<l.size()<<endl;
+
+    // ! print begin
+    // cout <<"begin= "<< *l.begin() <<endl;
+    for(auto it= l.begin();it!=l.end();it++){ //iterate bcz it have no index
+        cout<<*it<<endl;
+    }
+    cout<< "print-2";
+
+
+    for(int val:l){
+        cout<<val<<endl;
+    }
+
+    return 0;
+}
